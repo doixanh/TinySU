04f534458c3112d97ed918fc1638fb2fed92c1e4
doixanh <doixanh@gmail.com>
2017-08-12 22:17:55 +0700

    First commit

---
+++ src/daemon/tinysu.c
@@ -0,0 +1,12 @@
+/*
+ * This is 'su' source, on the C side.
+ * Licensed with GPLv3.
+ */
+
+/*
+ * We have two modes: daemon and launch
+ *  - daemon must be execute with root right (i.e. with prerooted insecure kernels)
+ *  - launch as normal
+ */
+
+
