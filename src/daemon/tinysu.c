dbafa4a9ade4de316b64a03fba86e070b196e6e2
doixanh <doixanh@yahoo.com>
2017-08-12 22:40:21 +0700

    Add command line parser

--- src/daemon/tinysu.c
+++ src/daemon/tinysu.c
@@ -9,9 +9,34 @@
  *  - launch as normal
  */
 
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void printUsage(char *self) {
+    fprintf(stderr, "Usage: %s [-h] [-d] [-c command]\n", self);
+    exit(1);
+}
+
+void goDaemonMode() {
+    printf("I'm going to daemon mode.\n");
+    exit(0);
+}
 
 int main(int argc, char **argv) {
+    int opt = 0;
 	printf("This is TinySU.\n");
+    while ((opt = getopt(argc, argv, "hd")) != -1) {
+        switch(opt) {
+            case 'h':
+                printUsage(argv[0]);
+                break;
+            case 'd':
+                goDaemonMode();
+                break;
+            default: /* '?' */
+                printUsage(argv[0]);
+        }
+    }
+    printUsage(argv[0]);
 }
