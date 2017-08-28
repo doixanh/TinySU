dea35b6bf1f7bf45e879534ca5b961ff8809fdad
doixanh <doixanh@yahoo.com>
2017-08-28 23:11:10 +0700

    app: Cleanup unnecessary activity. Theme the request activity to look like a dialog. I should have done better, though.

--- src/app/TinySUFrontend/src/main/java/com/doixanh/tinysu/RequestActivity.java
+++ src/app/TinySUFrontend/src/main/java/com/doixanh/tinysu/RequestActivity.java
@@ -2,6 +2,7 @@ package com.doixanh.tinysu;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -17,11 +18,20 @@ public class RequestActivity extends Activity {
         Intent intent = getIntent();
         int uid = intent.getIntExtra("uid", 0);
         String names[] = getPackageManager().getPackagesForUid(uid);
+
         String all = "";
+        PackageManager packageManager= getApplicationContext().getPackageManager();
+        try {
+            String appName = (String) packageManager.getApplicationLabel(packageManager.getApplicationInfo(names[0], PackageManager.GET_META_DATA));
+            all += appName + "\n\n";
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
         for (String name: names) {
             all += name + "\n";
         }
         TextView tv = (TextView) findViewById(R.id.uid);
-        tv.setText("Requesting root permission \n" + all);
+        tv.setText(all);
     }
 }
